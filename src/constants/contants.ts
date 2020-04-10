7362716e52a960ac99feb2867f5e2b596626eadb
adoi <adonismurati@gmail.com>
2020-04-10 23:08:50 +0200

    Provide multicopy constants for errors, information and overall snippet indo

---
+++ src/constants/contants.ts
@@ -0,0 +1,16 @@
+// Content for information boxes on the main window
+export const INFORMATION = {
+  SNIPPET_ADDED: '[muticopy] Your selection was added to the stack.',
+  SNIPPETS_REMOVED: '[multicopy] All selections were removed from the stack.'
+};
+
+// Content for error boxes on the main window
+export const ERROR = {
+  SNIPPETS_EMPTY: '[multicopy] No items in the collection',
+  NO_FILE: '[multicopy] No snippet was added in the stack. No file open'
+};
+
+// Content for snippet information in the intellisense boxes
+export const SNIPPET = {
+  HEADER: '=> Code to be inserted: '
+};
